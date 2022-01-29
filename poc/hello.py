41518b6e6547e57f1da58228339fbec840971efa
RainbowUnicorn7297 <67099591+RainbowUnicorn7297@users.noreply.github.com>
2022-01-29 18:31:47 +0800

    SSL test

--- poc/hello.py
+++ poc/hello.py
@@ -1,9 +1,10 @@
 def application(environ, start_response):
+    print('\n'.join([str(('%s: %s' % (key, value)).encode('utf-8'))
+                     for key, value in environ.items()]))
+    
     status = '200 OK'
     headers = [('Content-Type', 'text/html')]
     
     start_response(status, headers)
 
-    print('\n'.join([str(('%s: %s' % (key, value)).encode('utf-8'))
-                     for key, value in environ.items()]))
     return [b'<h1>Hello, web!</h1>']
--- poc/server.py
+++ poc/server.py
@@ -1,6 +1,13 @@
 from wsgiref.simple_server import make_server
+from ssl import wrap_socket
 from hello import application
 
 with make_server('', 443, application) as httpd:
-    print('Serving HTTP on port 443...')
+    certfile = '../key/server.crt'
+    keyfile = '../key/server.key'
+    httpd.socket = wrap_socket(
+        httpd.socket, certfile=certfile, keyfile=keyfile, server_side=True)
+    httpd.socket.accept()
+    
+    print('Serving HTTPS on port 443...')
     httpd.serve_forever()
